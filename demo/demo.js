d8cfc2d35b3805a490a4a6bed47afc10c5e9e820
Ian Collins <ikcollins@gmail.com>
2016-12-01 14:11:15 -0500

    add override object function and commenting

--- demo/demo.js
+++ demo/demo.js
@@ -573,6 +573,8 @@ module.exports = HtmlEditor;
 /*globals module*/
 'use strict';
 class Helper {
+  // return an object with all attributes from both objects,
+  // and 2nd object overrides 1st object values
   static mergeObjs(obj1, obj2) {
     var obj3 = {},
         attrname;
@@ -582,6 +584,19 @@ class Helper {
     return obj3;
   }
 
+  // override values in obj1 where the attr is the same in obj2
+  static objOverrideValues(obj1, obj2) {
+    var attrname;
+
+    for (attrname in obj2) {
+      if (attrname in obj1) {
+        obj1[attrname] = obj2[attrname];
+      }
+    }
+    return obj1;
+  }
+
+  // element has class name
   static hasClass(el, className) {
     var reg;
 
@@ -593,6 +608,7 @@ class Helper {
     }
   }
 
+  // add class to element
   static addClass(el, className) {
     if (el.classList) {
       el.classList.add(className);
@@ -601,6 +617,7 @@ class Helper {
     }
   }
 
+  // remove class from element
   static removeClass(el, className) {
     var reg;
 
@@ -612,6 +629,7 @@ class Helper {
     }
   }
 
+  // toggle class for element
   static toggleClass(el, className) {
     if (this.hasClass(el, className)) {
       this.removeClass(el, className);
@@ -620,6 +638,8 @@ class Helper {
     }
   }
 
+  // return 1st word preceeded by prefix in string
+  // ex: "re-" in "the re-creation of remix" returns "creation"
   static findWordWithPrefix(prefix, str) {
     var regex = new RegExp("\\b" + prefix + "(\\S+)", "gi"),
         match = str.match(regex);
